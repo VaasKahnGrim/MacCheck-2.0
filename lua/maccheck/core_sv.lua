8305a968f411dc00a1e1afefd6a92adfaea5faa0
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2021-02-14 09:23:44 -0600

    Billy Easter Egg

--- lua/maccheck/core_sv.lua
+++ lua/maccheck/core_sv.lua
@@ -2,5 +2,11 @@ util.AddNetworkString("MacCheck")
 
 
 net.Receive("MacCheck",function(len,pl)
-	pl:Kick("Get a real PC, not a Piece of Shit MAC.")
+	if math.random(0,1) == 0 then
+		pl:Kick("`SHIT-OS DONT SUPPORTING!!!` - Billy Feb 14th, 2021")
+	else
+		pl:Kick("Get a real PC, not a Piece of Shit MAC.")
+	end
 end)
+
+
