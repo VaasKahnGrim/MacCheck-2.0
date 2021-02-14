44f5b22d5bc8a39d3fcfa87a4189406dd3948b4a
Vaas_Kahn_Grim <29955667+VaasKahnGrim@users.noreply.github.com>
2021-02-14 09:25:49 -0600

    Billy Easter Egg

--- lua/maccheck/core_sv.lua
+++ lua/maccheck/core_sv.lua
@@ -1,8 +1,9 @@
 util.AddNetworkString("MacCheck")
 
+local TimeString = os.date("%d/%m",os.time())
 
 net.Receive("MacCheck",function(len,pl)
-	if math.random(0,1) == 0 then
+	if TimeString == "14/02" then
 		pl:Kick("`SHIT-OS DONT SUPPORTING!!!` - Billy Feb 14th, 2021")
 	else
 		pl:Kick("Get a real PC, not a Piece of Shit MAC.")
